8b7ecafb24a50a6e7a49503c8abf8ddaed3f5ced
Milk Wee <1140484715@qq.com>
2025-10-14 22:19:51 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +12,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -39,7 +45,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+        // 对前端传过来的明文密码进行md5加密处理
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
@@ -52,6 +59,39 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    /**
+     * 新增员工
+     * @param empolyeeDTO
+     */
+    @Override
+    public void save(EmployeeDTO empolyeeDTO) {
+//        System.out.println("当前线程的id" + Thread.currentThread().getId());
+
+        Employee employee = new Employee();
+//        employee.setName(empolyeeDTO.getName());//一个一个属性获取太长、太麻烦
+        //对象属性拷贝
+        BeanUtils.copyProperties(empolyeeDTO, employee);//注意顺序，第一个参数拷贝给第二个参数
+        //拷贝只有部分属性，其他属性需要手动设置
+
+        //设置账号状态，默认正常状态1表示正常 0表示锁定
+        employee.setStatus(StatusConstant.ENABLE);//不使用1或者0做参数，使用常量类方便后面修改
+
+        //设置默认密码123456，使用md5加密
+//        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));//此处密码写死了，默认密码也有常量配置
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+
+        //设置当前记录的创建时间和更新时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        //设置创建人id，更新人id，目前写固定值，后期需要完善
+        //TODO 后期改为当前登录用户的id
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+
+        employeeMapper.insert(employee);
+
     }
 
 }
