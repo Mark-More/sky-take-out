b1109f65a594bed2aaa5d171beaaa5f86d52d1d0
Milk Wee <1140484715@qq.com>
2025-11-13 22:08:58 +0800

    导出运营数据功能代码

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 /**
@@ -91,5 +92,13 @@ public class ReportController {
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
         log.info("销量排名数据统计：{}到{}",begin,end);
         return Result.success(reportService.getSalesTop10(begin,end));
+    }
+    /**
+     * 导出运营数据报表
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出运营数据报表")
+    public void export(HttpServletResponse  response){
+        reportService.exportBusinessData(response);
     }
 }
